6735bbdd73cdcff1639251681cedc2898993d812
Your Name <vanluu2009@gmail.com>
2021-09-28 18:24:05 -0700

    Dong ho python!

--- index.py
+++ index.py
@@ -4,13 +4,16 @@ from tkinter.ttk import*
 from time import strftime
 root = Tk()
 root.title("Đồng Hồ Tuyệt Vời By Thành!")
+
+
 def clock():
     string = strftime('%H:%M:%S:%p')
     label.config(text=string)
-    label.after(1000,clock)
+    label.after(1000, clock)
 
 
-label =Label(root, font = ("Digital-7",100) , background ='black', foreground = 'green')
+label = Label(root, font=("Digital-7", 100),
+              background='black', foreground='green')
 label.pack(anchor='center')
 clock()
 root.mainloop()
